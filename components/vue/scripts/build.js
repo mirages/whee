48d5e92074f6556ac2385eaef23733a1b9826831
mirages <18603728766@163.com>
2021-03-07 11:26:35 +0800

    feat(vue/loading): add loading vue component

---
+++ components/vue/scripts/build.js
@@ -0,0 +1,53 @@
+const { rollup } = require('rollup')
+const fs = require('fs')
+const path = require('path')
+const { babel } = require('@rollup/plugin-babel')
+const postcss = require('rollup-plugin-postcss')
+
+const build = async () => {
+  const dir = path.resolve(__dirname, '../src')
+  const files = await fs.promises.readdir(dir)
+
+  files.forEach(async file => {
+    try {
+      const componentPath = path.resolve(dir, file)
+      const stats = await fs.promises.stat(componentPath)
+      const input = path.resolve(componentPath, 'index.tsx')
+
+      if (!stats.isDirectory()) return
+      if (!fs.existsSync(input)) return
+
+      const distDir = path.resolve(__dirname, '../es', file)
+      const bundle = await rollup({
+        input,
+        plugins: [
+          babel({
+            extensions: ['.ts', '.tsx', '.js', '.jsx'],
+            babelHelpers: 'inline',
+            sourceMaps: false,
+            skipPreflightCheck: true
+          }),
+          postcss({
+            // css module
+            extensions: ['.css', '.less'],
+            autoModules: false, // 关闭自动 css module，使用自定义的 css module
+            modules: {
+              generateScopedName: '[local]__[contenthash:base64:8]'
+            },
+            extract: path.resolve(distDir, 'index.css')
+          })
+        ]
+      })
+
+      await bundle.write({
+        dir: distDir,
+        format: 'es'
+      })
+      await bundle.close()
+    } catch (e) {
+      console.log(e)
+    }
+  })
+}
+
+build().catch(console.error)
---
+++ components/vue/src/loading/index.tsx
@@ -0,0 +1,92 @@
+import {
+  defineComponent,
+  computed,
+  watch,
+  reactive,
+  isReactive,
+  ref,
+  PropType
+} from 'vue'
+import styles from './index.less'
+
+export type LoadingType = 'spinner' | 'circle'
+
+export default defineComponent({
+  name: 'wh-loading',
+  props: {
+    size: {
+      type: [Number, String],
+      default: 20
+    },
+    vertical: Boolean,
+    type: {
+      type: String as PropType<LoadingType>,
+      default: 'spinner'
+    },
+    color: {
+      type: String,
+      default: '#999'
+    }
+  },
+  setup(props, { slots }) {
+    const size = computed(() => Math.floor(Number(props.size) / 2) * 2)
+    const lineWidth = computed(() => (size.value * 0.5) / (1 + 0.6))
+    const lineHeight = computed(() => lineWidth.value / 3)
+    const renderSpinner = () => {
+      const num = 12 // 线条数，同 css 中的 animation-timing-function: steps(12)
+      const pos = []
+      const diffR = 360 / num
+      const diffO = 0.7 / num
+
+      for (let i = 0; i < num; i++) {
+        pos.push(
+          <i
+            style={{
+              width: `${Math.floor(lineWidth.value)}px`,
+              height: `${Math.floor(lineHeight.value)}px`,
+              transform: `translate(60%, -50%) rotate(${-90 + diffR * i}deg)`,
+              transformOrigin: '-60% 50%',
+              opacity: 1 - diffO * i,
+              backgroundColor: props.color
+            }}
+          ></i>
+        )
+      }
+
+      return pos
+    }
+    const renderCircle = () => {
+      return (
+        <svg viewBox="0 0 30 30">
+          <circle cx="15" cy="15" r="12" stroke={props.color}></circle>
+        </svg>
+      )
+    }
+
+    return () => (
+      <div
+        class={{
+          [styles.loading]: true,
+          [styles.vertical]: props.vertical
+        }}
+      >
+        <div
+          class={styles[props.type]}
+          style={{
+            width: size.value + 'px',
+            height: size.value + 'px'
+          }}
+        >
+          {props.type === 'spinner' ? renderSpinner() : renderCircle()}
+        </div>
+        <div
+          style={{
+            color: props.color
+          }}
+        >
+          {slots.default && slots.default()}
+        </div>
+      </div>
+    )
+  }
+})
